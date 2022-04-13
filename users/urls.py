b125dfb52e7d5e0b875c54d1607570e87e7aef17
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-04-14 02:03:02 +0530

    feat: add auth views

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,6 @@ from django.urls import URLPattern, path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
-    path('register/', views.register, name='register'),
+    path('login', views.login_user, name='login'),
+    path('register', views.register, name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,35 +1,39 @@
-import json
-from django.shortcuts import render
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, get_user_model, login
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from json import loads
 
-import users
-
+User = get_user_model()
 
 @csrf_exempt
-def login(request):
-    data = json.loads(request.body)
-    email = data.get('email', None)
+def login_user(request):
+    data = loads(request.body)
+    username = data.get('username', None)
     password = data.get('password', None)
-    if email is None or password is None:
-        return JsonResponse({'error': 'Missing email or password'})
-    user = authenticate(email=email, password=password)
-    print(email,password,user)
+
+    if username is None or password is None:
+        return JsonResponse({ 'error': 'Missing email or password' })
+
+    user = authenticate(request, username=username, password=password)
+    print(username, password,user)
+
     if user is not None:
         login(request, user)
-        return JsonResponse({"message": "You are logged in."})
-    return JsonResponse({"message": "Invalid credentials."})
+        return JsonResponse({ 'message': 'You are logged in.', 'token': user.token })
+    return JsonResponse({ 'message': 'Invalid credentials.' })
 
 @csrf_exempt
 def register(request):
-    data = json.loads(request.body)
+    data = loads(request.body)
     email = data.get('email', None)
     username = data.get('username', None)
     password = data.get('password', None)
     if email is None or password is None or username is None:
-        return JsonResponse({'error': 'Missing email or password'})
+        return JsonResponse({ 'error': 'Missing email or password' })
+
+    if User.objects.filter(username=username).exists():
+        return JsonResponse({ 'error': f'"{username}" already exists' })
+
     user = User.objects.create_user(username=username, email=email, password=password)
     user.save()
-    return JsonResponse({"message": "You are registered."})
+    return JsonResponse({ 'message': 'You are registered.' })
