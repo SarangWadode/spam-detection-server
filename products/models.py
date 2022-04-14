ea639a1e7d56cb58dd119ff184cc87df7e6a479f
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-04-14 10:45:49 +0530

    feat: add product details

--- products/models.py
+++ products/models.py
@@ -8,7 +8,7 @@ User = get_user_model()
 class Product(models.Model):
     name = models.CharField(max_length=30, verbose_name='Product Title')
     description = models.TextField()
-    image = models.ImageField(verbose_name='Product image', upload_to='uploads')
+    image = models.ImageField(verbose_name='Product image')
 
     def __str__(self) -> str:
         return f'''
@@ -17,10 +17,19 @@ class Product(models.Model):
         {self.description}
         '''
 
+    @property
+    def as_json(self):
+        return { 
+            'title': self.name,
+            'description': self.description,
+            'pk': self.pk,
+            'image': self.image.url
+        }
+
     @classmethod
     def get_products(cls, query='', start=0, count=10):
         result_query = models.Q(name__icontains=query) | models.Q(description__icontains=query)
-        filtered_products = cls.objects.filter(result_query)[start:start + count].values()
+        filtered_products = cls.objects.filter(result_query)[start:start + count].values('name', 'image', 'pk')
         return create_json(filtered_products, start)
 
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.http.request import HttpRequest
 from django.http.response import JsonResponse
+from django.shortcuts import get_object_or_404
 from .models import Comment, Order, Product
 
 
@@ -8,3 +9,8 @@ def product_list(request: HttpRequest) -> JsonResponse:
     start = request.GET.get('start', 0)
     count = request.GET.get('count', 10)
     return JsonResponse(Product.get_products(search, start, count))
+
+def product_detail(request: HttpRequest, pk: int) -> JsonResponse:
+    product = get_object_or_404(Product, pk=pk)
+    data = product.as_json
+    return JsonResponse( { **data, 'image': request.build_absolute_uri(data['image']) })
