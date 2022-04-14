9f18cb0b4aa07bfda828e623a60e2a5f5abe2751
Mukesh Gurpude <mukeshgurpude02@gmail.com>
2022-04-14 11:47:22 +0530

    chore: add base media url

--- products/utils.py
+++ products/utils.py
@@ -1,9 +1,8 @@
 from django.db.models import QuerySet
 
-
 def create_json(results: QuerySet, start: int):
     return {
-        'results': list(results),
         'start': start,
-        'count': results.count()
+        'count': results.count(),
+        'results': list(results)
     }
--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.http.request import HttpRequest
 from django.http.response import JsonResponse
 from django.shortcuts import get_object_or_404
+from django.conf import settings
 from .models import Comment, Order, Product
 
 
@@ -8,7 +9,10 @@ def product_list(request: HttpRequest) -> JsonResponse:
     search = request.GET.get('search', '')
     start = request.GET.get('start', 0)
     count = request.GET.get('count', 10)
-    return JsonResponse(Product.get_products(search, start, count))
+    return JsonResponse({ 
+        'base_url': request.build_absolute_uri(settings.MEDIA_URL),
+        **Product.get_products(search, start, count)
+    })
 
 def product_detail(request: HttpRequest, pk: int) -> JsonResponse:
     product = get_object_or_404(Product, pk=pk)
